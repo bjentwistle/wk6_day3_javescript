aed57bba2935630321d5246daf86dbf1f557bcaa
Becky <bjentwistle@gmail.com>
2023-05-10 16:19:22 +0100

    final tests added to taxi_specs.js

--- testing_taxi/specs/taxi_specs.js
+++ testing_taxi/specs/taxi_specs.js
@@ -21,14 +21,36 @@ describe('Taxi', function() {  //grouping in describe
     })
 
     it('should have a driver', function(){
-        const actual = taxi.driver
-        assert.strictEqual(actual, "Becky")
+        const actual = taxi.driver //actual
+        assert.strictEqual(actual, "Becky")  //assert 
     })
 
     describe('passengers', function(){
         it('should start with no passengers', function(){
-            const actual = taxi.passengers;
+            const actual = taxi.passengers;  //actual
             assert.deepStrictEqual(actual, []) //not exact same thing because of memory hence use deepStrictEqual
+        })
+        it('should be able to return number of passengers', function() {
+            const actual = taxi.numOfPassengers()  //actual
+            assert.strictEqual(actual, 0)  //strict equal as zero is zero
+        })
+
+        it('should be able to add a passenger', function(){
+            taxi.addPassengers("Charlie")  //need to create a function to add passenger
+            const actual = taxi.numOfPassengers() //actual now we have added Charlie
+            assert.deepStrictEqual(actual, 1)
+        })
+
+        it('should be able to remove passengers', function(){
+            taxi.addPassengers("Charlie")  
+            taxi.removePassengersByName("Charlie")
+            const actual = taxi.numOfPassengers() //actual now we have added Charlie
+            assert.deepStrictEqual(actual, 0)
+        })
+        it('should be able to remove all passengers', function() {
+            taxi.removeAllPassengers()
+            const actual = taxi.numOfPassengers()
+            assert.strictEqual(actual, 0)
         })
 
     })
--- testing_taxi/taxi.js
+++ testing_taxi/taxi.js
@@ -6,5 +6,24 @@ const Taxi = function(manufacturer, model, driver) {
     this.driver = driver
     this.passengers = [] //empty seats
 }
+Taxi.prototype.numOfPassengers = function() {  // create a method to get the length of the passengers array
+    return this.passengers.length;
+}
+Taxi.prototype.addPassengers = function(passenger) {
+    this.passengers.push(passenger);
+}
+
+Taxi.prototype.removePassengersByName = function(passenger) {
+    const index = this.passengers.indexOf(passenger)
+    this.passengers.splice(index, 1);
+}
+
+Taxi.prototype.removeAllPassengers = function() {
+    this.passengers = []
+}
+
+// removePassengerByName
+// removeAllPassengers
+// A passenger should be represented as a String containing the passenger's name.
 
 module.exports = Taxi;
