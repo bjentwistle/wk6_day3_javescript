08f2120fa521208e8445f64db95c5896c04970ef
Becky <bjentwistle@gmail.com>
2023-05-10 23:56:24 +0100

    finally finished mvp on homework

--- day_3_e64notes/hw_tdd_jurrasic_park/hw_tdd_jurassic_park_start/models/park.js
+++ day_3_e64notes/hw_tdd_jurrasic_park/hw_tdd_jurassic_park_start/models/park.js
@@ -1,11 +1,79 @@
 //Create Park blueprint
 
+
 const Park = function(name, ticketPrice) {
     this.name = name
     this.ticketPrice = ticketPrice
     this.collectionOfDinos = []
 };
 
-//["T-Rex", "Ankylosaurus", "Stegasaurus"]
+
+Park.prototype.numOfDinos = function() {
+    return this.collectionOfDinos.length
+};
+
+Park.prototype.addDinos = function(dinosaur) {
+    this.collectionOfDinos.push(dinosaur)
+};
+
+Park.prototype.deleteDinosaurBySpecies = function(dinoToRemove) {
+    const index = this.collectionOfDinos.indexOf(dinoToRemove)
+    this.collectionOfDinos.splice(index, 1)
+};
+
+//example of a loop in JS
+// const numbers = [1, 2, 3];
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+Park.prototype.highestAttractorDino = function(){
+    let highestAttractor = 0 //guest per day counter, starts at 0
+    let highestAttractorDino = null //make the dinosaur null for now
+
+    for (const dinosaur of this.collectionOfDinos){
+        if (dinosaur.guestsAttractedPerDay > highestAttractor) {   //checking the guests attracted by dino per day against the counter
+            highestAttractor =dinosaur.guestsAttractedPerDay  //need to update the guest per day counter
+            highestAttractorDino = dinosaur //the new highest attractor
+        }
+    } 
+    return highestAttractorDino
+};
+
+Park.prototype.selectAllBySpecies = function(species) {
+    let listOfDinosBySpecies = []
+    for (dinosaur of this.collectionOfDinos) {
+        //console.log("after for", dinosaur.species)
+        if (dinosaur.species === species) {
+            listOfDinosBySpecies.push(dinosaur)
+            //console.log("Species after if", dinosaur.species)
+        }
+    }
+    //console.log(listOfDinosBySpecies)
+    return listOfDinosBySpecies
+};
+
+Park.prototype.totalVisitorsPerDay = function(){
+    let visitorCount = 0
+    for (dinosaur of this.collectionOfDinos) {
+        visitorCount += dinosaur.guestsAttractedPerDay
+    }
+    return visitorCount
+};
+
+Park.prototype.revenuePerYear = function(){
+    let visitorCount = 0
+    for (dinosaur of this.collectionOfDinos) {
+        visitorCount += dinosaur.guestsAttractedPerDay
+    }
+    revenuePerYear = visitorCount*365*(this.ticketPrice)
+    return revenuePerYear
+};
+
+
+//don't need delete all!
+// Park.prototype.deleteAllDinosaurs = function () {
+//     this.collectionOfDinos = []
+// }
 
 module.exports = Park;
--- day_3_e64notes/hw_tdd_jurrasic_park/hw_tdd_jurassic_park_start/specs/park_spec.js
+++ day_3_e64notes/hw_tdd_jurrasic_park/hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -6,7 +6,7 @@ describe('Park', function () {
   let park;
 
   beforeEach(function () {
-    park = new Park("Jurassic Park", 500);
+    park = new Park("Jurassic Park", 500, ["T-Rex", "Ankylosaurus", "Stegasaurus"]);
   })
 
   it('should have a name', function() {
@@ -18,21 +18,92 @@ describe('Park', function () {
   const actual = park.ticketPrice;
   assert.strictEqual(actual, 500)
 });
+  describe('Dinosaur', function(){
+    let dinosaur
 
-  it('should have a collection of dinosaurs');
+  })
+  it('should have a collection of dinosaurs', function() {
+    dinosaur = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur)
+    dinosaur = new Dinosaur("Triceratops", "Herbivore", 40)
+    park.addDinos(dinosaur)
+    dinosaur = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur)
+    const actual = park.numOfDinos()
+    assert.strictEqual(actual, 3)
+  });
 
-  it('should be able to add a dinosaur to its collection');
+    it('should be able to add a dinosaur to its collection', function(){
+      dinosaur = new Dinosaur("Stegosaurus", "Herbivore", 25)
+      park.addDinos(dinosaur)
+      const actual = park.numOfDinos()
+      assert.strictEqual(actual, 1)
+    });
 
-  it('should be able to remove a dinosaur from its collection');
+    it('should be able to remove a dinosaur from its collection', function() {
+      dinosaur1 = new Dinosaur("T-Rex", "Carnivore", 50)
+      park.addDinos(dinosaur1)
+      dinosaur2 = new Dinosaur("Triceratops", "Herbivore", 40)
+      park.addDinos(dinosaur2)
+      park.deleteDinosaurBySpecies(dinosaur1);
+      const actual = park.numOfDinos()
+      assert.strictEqual(actual, 1)
+    });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    dinosaur = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur)
+    dinosaur1 = new Dinosaur("Triceratops", "Herbivore", 40)
+    park.addDinos(dinosaur1)
+    dinosaur2 = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur2)
+    const actual = park.highestAttractorDino()
+    assert.strictEqual(actual, dinosaur)
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species', function(){
+    dinosaur0 = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur0)
+    dinosaur1 = new Dinosaur("T-Rex", "Carnivore", 10)
+    park.addDinos(dinosaur1)
+    dinosaur2 = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur2)
+    const actual = park.selectAllBySpecies("T-Rex")
+    assert.deepStrictEqual(actual, [dinosaur0, dinosaur1])
+  });
 
-  it('should be able to calculate the total number of visitors per day');
+  it('should be able to calculate the total number of visitors per day', function(){
+    dinosaur = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur)
+    dinosaur1 = new Dinosaur("Triceratops", "Herbivore", 40)
+    park.addDinos(dinosaur1)
+    dinosaur2 = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur2)
+    const actual = park.totalVisitorsPerDay()
+    assert.strictEqual(actual, 120)
 
-  it('should be able to calculate the total number of visitors per year');
+  });
 
-  it('should be able to calculate total revenue for one year');
+  it('should be able to calculate the total number of visitors per year', function(){
+    dinosaur = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur)
+    dinosaur1 = new Dinosaur("Triceratops", "Herbivore", 40)
+    park.addDinos(dinosaur1)
+    dinosaur2 = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur2)
+    const dailyTotal = park.totalVisitorsPerDay()
+    const actual = (dailyTotal*365)
+    assert.strictEqual(actual, 43800)
+    });
 
+  it('should be able to calculate total revenue for one year', function(){
+    dinosaur = new Dinosaur("T-Rex", "Carnivore", 50)
+    park.addDinos(dinosaur)
+    dinosaur1 = new Dinosaur("Triceratops", "Herbivore", 40)
+    park.addDinos(dinosaur1)
+    dinosaur2 = new Dinosaur("Ankylosaurus", "Herbivore", 30)
+    park.addDinos(dinosaur2)
+    const actual = park.revenuePerYear()
+    assert.strictEqual(actual, 21900000)
+})
 });
